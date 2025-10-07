8c8561ac440ab699acf32e0c4c3e139ab5da92bd
didou <d.zebig@gmail.com>
2025-10-07 16:31:38 +0200

    init GameChange

---
+++ hs/src/main/groovy/org/zebig/hs/state/GameChange.groovy
@@ -0,0 +1,30 @@
+package org.zebig.hs.state
+
+class GameChange {
+    /*
+    TODO pubLic     PlayerBecomesActive(player_name)
+    TODO public     ManaStatusChanged(player_name, mana_status)
+    TODO private    CardDrawn(card_detail)
+    TODO private    CardCostChanged(card_detail)
+    TODO public     CardTransformed(old_card_detail, new_card_detail)
+    TODO public     MinionPlayed(card_detail, position, [target], [effect_chosen])
+    TODO public     MinionAttacks(card_detail, target, attack_power)
+    TODO public     MinionTakesDamage(card_detail, minion_health, damage_amount)
+    TODO public     MinionIsHealed(card_detail, minion_health, heal_amount)
+    TODO public     HeroTakesDamage(player_name, hero_health, damage_amount)
+    TODO public     MinionIsHealed(card_detail, minion_health, heal_amount)
+    TODO public     MinionDies(card_detail)
+    TODO public     DamageInflicted(target_detail)
+    TODO public     SpellPlayed(card_detail, [target], [effect_chosen]) note: non-secret spells
+    TODO private    SecretPlayed(card_detail)
+    TODO public     SecretRevealed(card_detail)
+    TODO public     WeaponReady(card_detail)
+    TODO public     WeaponDestroyed(card_detail)
+    TODO public     ZoneSizeChange(zone_name, new_size) deck, board, hand, secrets
+    TODO public     HealingReceived(target)
+    TODO public     HeroDies(player_name)
+    TODO public     HeroTakesFatigue(player_name, fatigue_amount, new_hero_health)
+    TODO public     TargetFreeze(target_detail)
+    TODO public     TargetUnFreeze(target_detail)
+     */
+}
